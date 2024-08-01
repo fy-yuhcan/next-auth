a3d2cda986503da7149820ebee3695c89334bf8d
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-08-01 20:40:29 +0900

    型定義、サイトの情報を保存

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig= {
+    name: "post writer",
+    description: "",
+    url: "http://localhost:3000",
+    ogImage:"",
+    links:{
+        x:"https://x.com/yunbo_yuh",
+        github:"https://github.com/fy-yuhcan"
+    }
+    
+}
