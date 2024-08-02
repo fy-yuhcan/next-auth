93411e74fc0fa086b7b5ed9f1133d69f4bea6488
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-08-02 18:02:51 +0900

    contactの作成

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -105,8 +105,22 @@ export default function IndexPage(){
                     </div>
                 </div>
               </div>
+              <div className="mx-auto md:max-w-[58rem] text-center">
+                    <p className="text-muted-foreground sm:text-lg sm:leading-7">Post Writerはログインするとブログ投稿することができます。
+                    </p>
+                </div>
             </section>
-            <section></section>
+
+            <section id="contact" className="container py-8 md:py-12 lg:py-24">
+                <div className="max-w-[58rem] mx-auto text-center flex flex-col gap-4">
+                    <h2 className="font-extrabold text-3xl md:text-4xl lg:text-5xl">Contact Me</h2>
+                    <p className="text-muted-foreground sm:text-lg sm:leading-7">もしwebサービスが気に入った場合は下記XからDMでご連絡ください。
+                    <br />
+                    お仕事のご連絡をお待ちしています。
+                    </p>
+                    <Link href={siteConfig.links.x} className="underline-offset-4" target="_blank" rel="noreferrer">Xまで</Link>
+                </div>
+            </section>
         </div>
     )
 }
