0f95988289682d9e9086e9744609a4753ed95126
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-08-03 01:38:12 +0900

    uselockbodycontrollのカスタムフックの追加

--- components/mobileNav.tsx
+++ components/mobileNav.tsx
@@ -1,5 +1,6 @@
 import { siteConfig } from "@/config/site";
 import { NavItem } from "@/types";
+import { useLockBodyScroll } from "@uidotdev/usehooks";
 import Link from "next/link";
 
 //itemsの型を定義する
@@ -9,6 +10,9 @@ type MobileNavProps = {
 }
 
 export default function MobileNav({items}: MobileNavProps) {
+    {/*モーダルが展開しているときに後ろの画面が動かなくなるカスタムフック*/}
+    useLockBodyScroll();
+
     return (
         <div className="fixed top-16 left-0 right-0 bottom-0 z-50 p-6 shadow-md md:hidden animate-in slide-in-from-bottom-80">
             <div className="grid gap-6 bg-popover p-4 text-popover-foreground">
