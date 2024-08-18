ea602caf653ea968ad565c8c9d8bd5dc5c286957
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-08-18 23:55:03 +0900

    ブログ取得

---
+++ app/(marketing)/blog/page.tsx
@@ -0,0 +1,11 @@
+import { allPosts } from "@/.contentlayer/generated"
+
+export default function BlogPage(){
+    const posts = allPosts
+    console.log(posts)
+  return (
+    <div>
+    Blogpage
+    </div>
+  )
+}
