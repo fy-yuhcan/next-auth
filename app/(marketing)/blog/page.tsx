7e2baa127dc6d66d4dfbe196680e1e3579565e7e
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-08-19 00:25:20 +0900

    postを取得して表示する

--- app/(marketing)/blog/page.tsx
+++ app/(marketing)/blog/page.tsx
@@ -1,20 +1,29 @@
 import { allPosts } from "@/.contentlayer/generated"
+import Image from "next/image";
 
 export default function BlogPage(){
     const posts = allPosts
     console.log(posts);
   return (
+    <div className="container max-w-4xl py-6 lg:py-10">
     <div>
-    <div>
-        <div>
-            <h1>
+        <div className="space-y-4">
+            <h1 className="font-extrabold text-4xl lg:text-5xl tracking-tight">
                 Blog
             </h1>
-            <p>
+            <p className="text-muted-foreground text-xl">
                 contentlayerとmdxで書いています
             </p>
         </div>
     </div>
+    <hr className="my-8"/>
+    {posts.map((post,index)=>(
+        <article key={index}>
+            {post.image && <Image src={post.image} width={800} height={452} alt="Blog Image"/>}
+            <h2 className="text-2xl font-extrabold">{post.title}</h2>
+            <p className="text-muted-foreground">{post.description}</p>
+        </article>
+    ))}
     </div>
   )
 }
