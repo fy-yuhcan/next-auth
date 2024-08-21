70b6c4241dc8865dc0a50256a437c55b30e82e55
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-08-21 23:51:47 +0900

    認証フォーム作成

---
+++ components/icon.ts
@@ -0,0 +1,6 @@
+import { Github, Loader2 } from "lucide-react";
+
+export const Icon = {
+    spinner: Loader2,
+    github: Github
+}
--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -2,19 +2,31 @@ import { cn } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { buttonVariants } from "./ui/button";
+import { Icon } from "./icon";
+
 
 export default function UserAuthForm() {
     return (
         <div className="grid gap-2">
-            <form className="grid gap-1">
-                <div>
+            <form>
+                <div className="grid gap-1">
                     <div>
                         <Label htmlFor="email">メールアドレス</Label>
                         <Input id="email" placeholder="name@example.com" type="email"/>
                     </div>
-                    <button className={cn(buttonVariants())}>メールアドレスでログイン</button>
+                    <button className={cn(buttonVariants(),"w-full")}>メールアドレスでログイン</button>
                 </div>
             </form>
+            <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                    <span className="w-full border-t"></span>
+                </div>
+                <div className="relative flex justify-center text-xs ">
+                    <span className="text-muted-foreground px-2 bg-background">または</span>
+                </div>
+            </div>
+
+            <button className={cn(buttonVariants({variant:"outline"}))}><Icon.github></Icon.github>GitHub</button>
         </div>
     )
 }
